3ffc4bd5dacf070c33ace54ee7d979d5e0ffff7f
Lyes HAMIDOUCHE <lyes@padam.io>
2019-12-17 23:52:04 +0100

    Update

--- pres_2/signals.py
+++ pres_2/signals.py
@@ -3,6 +3,7 @@ import os
 import time
 import  signal
 
+
 NB_PROCS = 4
 
 received_sigints = 0
@@ -11,8 +12,10 @@ received_sigints = 0
 # In this example the queues that exist are FIFO, we can also use ProritizedQueues and LIFO queues
 
 def sigint_handler(signal, frame):
-    print("Signal received", signal, )
-    received_sigints+=1
+    print("Signal received", signal,frame)
+    frame.f_globals["received_sigints"]+=1
+    if frame.f_globals["received_sigints"] > 4:
+        exit(0)
 
 
 
@@ -20,7 +23,7 @@ if __name__ == '__main__':
     print("{} start workers ...".format(os.getpid()))
     finished_processes = [] * NB_PROCS
 
-    signal(signal.SIGINT, sigint_handler)
+    signal.signal(signal.SIGINT, sigint_handler)
     children = []
 
 
